14de7e98216f411179ac3b7c0db2f7a8368c05a9
Kusumita <29kusumita@gmail.com>
2025-03-01 19:05:31 +0530

    documentation and internationalisation

--- microservice/src/main/java/com/kusumita/rest_api/microservice/helloworld/HelloWorldController.java
+++ microservice/src/main/java/com/kusumita/rest_api/microservice/helloworld/HelloWorldController.java
@@ -1,5 +1,10 @@
 package com.kusumita.rest_api.microservice.helloworld;
 
+import java.util.Locale;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.MessageSource;
+import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
@@ -7,6 +12,9 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 public class HelloWorldController {
 
+	@Autowired
+	private MessageSource messageSource;
+	
 	@GetMapping(path = "/hello-world")
 	public String helloWorld() {
 		return "Hello World";
@@ -20,5 +28,11 @@ public class HelloWorldController {
 	@GetMapping(path = "/hello-world/{name}")
 	public HelloWorldBean helloWorldPathParam(@PathVariable String name) {
 	    return new HelloWorldBean("Hello World " + name);
+	}
+	
+	@GetMapping(path = "/hello-world-i18n")
+	public String helloWorldi18n() {
+		Locale locale = LocaleContextHolder.getLocale();
+		return messageSource.getMessage("good.morning.message", null, "Default Message", locale);
 	}
 }
